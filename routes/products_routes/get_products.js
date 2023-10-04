8166817c8e438e9e8fb2f00eb8f92d24fe3423e2
ChandanKumar Singh <cks917356@gmail.com>
2023-10-04 08:45:48 +0530

    products api

--- routes/products_routes/get_products.js
+++ routes/products_routes/get_products.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router() 
 const {productsList,addProduct,searchProducts} = require('./../../controller/product_controller.js')
 
-router.get('/products/:name',productsList)
+router.get('/products/',productsList)
 router.post('/add',addProduct)
 router.get('/search-product/:searchkey',searchProducts)
 
